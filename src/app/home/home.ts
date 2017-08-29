4cbd9e8feb9b23ad48d350d9e4924f5503b49e60
Alexander Zhidkov <alexander.zhidkov@empired.com>
2017-08-29 21:41:34 +0800

    from, until date pickers on home page

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import * as moment from 'moment';
 
 @Component({
     selector: 'app-home',
@@ -6,4 +8,20 @@ import { Component } from '@angular/core';
     styleUrls: ['./home.css'],
 })
 
-export class HomeComponent { }
+export class HomeComponent implements OnInit {
+    from: Date;
+    until: Date;
+
+    futureDatesOnly = (d: Date): boolean => {
+        const day = d.getDay();
+        return moment(Date.now()).diff(d, 'days') < 1 && day !== 0 && day !== 6;
+    }
+
+    constructor(private db: AngularFireDatabase) { }
+
+    ngOnInit() {
+        this.from = new Date();
+        this.until = new Date();
+    }
+
+}
